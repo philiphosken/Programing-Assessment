77daaa8709e2229c38f3caaa08ee8f1dbc8f73bd
Philip Hosken <philip.hosken@uon.edu.au>
2019-04-03 23:39:18 +0000

    Complete now with detailed Comments

--- main.c
+++ main.c
@@ -4,24 +4,24 @@ int main()
 {
 char alphatxt[] = "TEST"; //Text that is to become encrypted
 int N = 4; //Number of Values in the Array above
-int i; //Counter Index
-int k = -30; //KEY or the number of charcters shifted in the rotation cypher
+int i; //Counter Index in order to read alter and reprint each element in the array
+int k = 1; //KEY = the number of charcters shifted in the rotation cypher
 
-if (k > 26 || k < -26){
-    printf("%d is an invalid value for k. Select an interger between -25 and 25", k);
-} else
+if (k > 26 || k < -26){ //if the value of k was 26 or greater, it would either remain unchanged or thered be an equivelent key between -25 and 25
+    printf("%d is an invalid value for k. Select an interger between -25 and 25", k); //Prints an error message if tis is the case
+} else //If k is a valid number then continue to for loop
 
 
 for (i = 0 ; i < N ; ++i){
-    int phi = alphatxt[i]; //place holding integer
+    int phi = alphatxt[i]; //phi = place holding integer. This whill store each number ASCII number in the array to the variable 'phi'
     
-    if ((phi + k) > 90){
-        printf("%c", phi - 26 + k); // if it try to shift to characters after 'Z' loop back to 'A'
+    if ((phi + k) > 90){ // If it exceeds 'Z' after k is added, then simply take 26 away then add k
+        printf("%c", phi - 26 + k); //shift to characters after 'Z' loop back to 'A'
         
-    } else if ( (phi + k) < 65 ){
+    } else if ( (phi + k) < 65 ){ //If it exceeds 'Z' after k is added, then simply add 26 away then add k
         printf("%c", phi + 26 + k);
             
-    } else printf("%c", phi + k);
+    } else printf("%c", phi + k);// if within the bounds of ASCII 'A' and 'Z' just add k
 }
       return 0;
 }

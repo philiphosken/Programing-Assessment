f6216814c6c8ec65dddcbf86e132f87fe0aed758
Philip Hosken <philip.hosken@uon.edu.au>
2019-04-03 12:51:34 +0000

    FINAL

--- main.c
+++ main.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 
-//encryp(int m, int k, int x)
-
 int main()
 {
-char alphatxt[] = "IMGAY"; //The Text that is going to become encrypted is going to be typed here//
-int i; //Counter
-int N = 5; //Number of Values in the Array
-int k = -1; //KEY or the number of charcters shifted in the rotation cypher
+char alphatxt[] = "TEST"; //Text that is to become encrypted
+int N = 4; //Number of Values in the Array above
+int i; //Counter Index
+int k = -30; //KEY or the number of charcters shifted in the rotation cypher
 
-//error message needed if k is bigger or smaller then 24 and -26
+if (k > 26 || k < -26){
+    printf("%d is an invalid value for k. Select an interger between -25 and 25", k);
+} else
+
 
 for (i = 0 ; i < N ; ++i){
     int phi = alphatxt[i]; //place holding integer
     
     if ((phi + k) > 90){
-        printf("%c", phi - 26 + k);
+        printf("%c", phi - 26 + k); // if it try to shift to characters after 'Z' loop back to 'A'
         
     } else if ( (phi + k) < 65 ){
         printf("%c", phi + 26 + k);
@@ -23,9 +24,4 @@ for (i = 0 ; i < N ; ++i){
     } else printf("%c", phi + k);
 }
       return 0;
-
 }
-
-//encryp(int m, int k, int x){
-//    x = (m + k) % 26;
-//}

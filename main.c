128fa1a6ee14f0b1615d242481ef39d1c27b62fa
Philip Hosken <philip.hosken@uon.edu.au>
2019-04-03 08:51:28 +0000

    Progress at last... This works for the word that im encrypting however it needs to loop back to 'A' after the letter 'Z'

--- main.c
+++ main.c
@@ -1,7 +1,25 @@
-#include<stdio.h>
+#include <stdio.h>
 
-main()
+//encryp(int m, int k, int x)
+
+int main()
 {
-love it when it says main.c 
+char alpha[] = "TEST"; ///The Text that is going to become encrypted is going to be typed here///
+int i;
+int N = 4;
+int k = -1;
+
+for (i = 0 ; i < N ; ++i){
+    printf("%c", alpha[i] + k);
+    
+    
+}
+
+
+return 0;
 
 }
+
+//encryp(int m, int k, int x){
+//    x = (m + k) % 26;
+//}

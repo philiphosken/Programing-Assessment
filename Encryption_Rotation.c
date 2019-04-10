31e4307d26b7105a4552440b3150c42a8bcd0b85
Philip Hosken <philip.hosken@uon.edu.au>
2019-04-10 12:42:19 +0000

    Final commit...All further under "c3331534_Assessment3"

--- Encryption_Rotation.c
+++ Encryption_Rotation.c
@@ -6,30 +6,30 @@ int main()
 int k = 1; //KEY = the number of charcters shifted in the rotation cypher to generate coded message 
 
 
-    FILE *message;
-    FILE *coded;
+    FILE *in;
+    FILE *out;
     
-    message = fopen("message.txt", "r");
-    coded = fopen("coded.txt", "w");
+    in = fopen("in.txt", "r");
+    out = fopen("out.txt", "w");
     
-    while (feof(message) == 0){
+    while (feof(in) == 0){
         char phi;
-        fscanf(message, "%c", &phi);
+        fscanf(in, "%c", &phi);
       
           
     if (phi == 32){
-     fprintf(coded, "%c", phi); //Just prints the space
+     fprintf(out, "%c", phi); //Just prints the space
      printf("%c", phi);
     
     }else if((phi + k) > 90){ // If it exceeds 'Z' after k is added, then simply take 26 away then add k
-        fprintf(coded, "%c", phi - 26 + k); 
+        fprintf(out, "%c", phi - 26 + k); 
         printf("%c", phi - 26 + k);
         
     } else if ( (phi + k) < 65 ){ //If it exceeds 'Z' after k is added, then simply add 26 away then add k
-        fprintf(coded, "%c", phi + 26 + k);
+        fprintf(out, "%c", phi + 26 + k);
         printf("%c", phi + 26 + k);
             
-    } else { fprintf(coded, "%c", phi + k);// if within the bounds of ASCII 'A' and 'Z' just add k
+    } else { fprintf(out, "%c", phi + k);// if within the bounds of ASCII 'A' and 'Z' just add k
    printf("%c", phi + k);
    }
 }
